daa96d4d079eec19a82c1e2e8cb701d12055a3e7
wallanmota <wallanmota.13@gmail.com>
2021-08-20 15:30:50 -0300

    estrutura inicial do site

---
+++ js/script.js
@@ -0,0 +1,22 @@
+    var _AlturaDocumento = $(window).height(); //pega altura da janela
+
+    function AlturaSecao() {
+      $('.secao').height(_AlturaDocumento); //atribui o valor da variável a todas as divs com a classe secao
+    }
+
+    function ScrollSecao() {
+      console.log('entrou');
+      var body = $('html, body');
+      if($(this).parent('.secao').is('#secao-01')) {
+        //verifica se está na primeira seção e rola para a segunda
+        body.animate({scrollTop: _AlturaDocumento}, '500');
+      } else {
+        //rola para o início da página
+        body.animate({scrollTop: 0}, '800');
+      }
+    }
+
+    $('.icone-scroll').click(ScrollSecao); //chama a função na ação de click no ícone
+
+    $(AlturaSecao); //chama a nossa função só depois que o documento estiver completamente carregado
+  
